fd324cfabbc8ee1b7d56a3407bf1cc79a7461261
MOJAD - GOOS <eliekwizera030@gmail.com>
2026-06-01 19:32:25 +0200

    Create index.ts
    feat(services): unify service layer exports

---
+++ prisma/api/core/services/index.ts
@@ -0,0 +1,6 @@
+export * from "./pi-auth-service";
+export * from "./pi-wallet-service";
+export * from "./pi-payment-service";
+export * from "./job-service";
+export * from "./business-service";
+export * from "./investor-service";

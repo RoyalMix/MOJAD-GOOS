60042854b6c772ddd99e3e8dfc8b1087a5f67204
MOJAD - GOOS <eliekwizera030@gmail.com>
2026-06-02 11:45:41 +0200

    Create match.engine.ts
    feat(ai): add match engine core

---
+++ prisma/services/search-service/api/v1/search/core/ai-engine/match.engine.ts
@@ -0,0 +1,21 @@
+export class MatchEngine {
+
+matchUserToJobs(user:any,jobs:any[]){
+
+return [];
+
+}
+
+matchInvestorToStartups(investor:any,startups:any[]){
+
+return [];
+
+}
+
+matchBusinessPartners(business:any,partners:any[]){
+
+return [];
+
+}
+
+}

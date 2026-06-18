e0fea1f9c92ea0ef2b592c18ead3b5cc46c7290f
MOJAD - GOOS <eliekwizera030@gmail.com>
2026-06-18 05:33:34 +0200

    Create index.ts
    git add .
    git commit -m "fix(structure): create root users api"
    git push origin main

---
+++ api/v1/users/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+  res.json({
+    id: req.params.id
+  });
+});
+
+export default router;

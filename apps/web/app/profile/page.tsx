8f9eae8b1db42a02ebf56cb55f3da2a5203fbca3
MOJAD - GOOS <eliekwizera030@gmail.com>
2026-06-22 10:27:51 +0200

    Create page.tsx
    git add .
    git commit -m "feat(profile): add profile page"
    git push origin main

---
+++ apps/web/app/profile/page.tsx
@@ -0,0 +1,13 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>MOJAD Profile</h1>
+
+      <p>MOJAD ID</p>
+
+      <p>Trust Score</p>
+
+      <p>Opportunity Score</p>
+    </div>
+  );
+}

51e5150ccd94bb54dea7e16615ba0baa74339ec7
MOJAD - GOOS <eliekwizera030@gmail.com>
2026-06-24 21:14:17 +0200

    Create page.tsx
    feat(wallet): add opportunity wallet dashboard

---
+++ apps/web/app/apps/web/app/wallet/page.tsx
@@ -0,0 +1,23 @@
+export default function WalletPage() {
+
+return (
+
+<div>
+
+<h1>Opportunity Wallet</h1>
+
+<p>Pi Balance</p>
+
+<p>Referral Earnings</p>
+
+<p>Service Earnings</p>
+
+<p>Creator Earnings</p>
+
+<p>Investment Earnings</p>
+
+</div>
+
+);
+
+}

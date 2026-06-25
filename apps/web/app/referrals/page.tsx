b518585b0bcc6a29a11f3f68d0137e1179900032
MOJAD - GOOS <eliekwizera030@gmail.com>
2026-06-25 10:40:55 +0200

    Create page.tsx
    feat(referrals): add referral economy dashboard

---
+++ apps/web/app/referrals/page.tsx
@@ -0,0 +1,19 @@
+export default function ReferralPage() {
+
+return (
+
+<div>
+
+<h1>Referral Economy</h1>
+
+<p>Invite users</p>
+
+<p>Track rewards</p>
+
+<p>Track referrals</p>
+
+</div>
+
+);
+
+}

dd8dc48951a0ed27005802576485b86a0ba39238
MOJAD - GOOS <eliekwizera030@gmail.com>
2026-06-25 19:15:10 +0200

    Create page.tsx
    feat(servicenet): add services marketplace

---
+++ apps/web/app/services/page.tsx
@@ -0,0 +1,17 @@
+Salon
+Barbershop
+Hair Stylist
+Makeup Artist
+Photographer
+Electrician
+Plumber
+Mechanic
+Teacher
+Graphic Designer
+Developer
+Farmer
+Driver
+Cleaner
+Security Guard
+Football 
+Games 

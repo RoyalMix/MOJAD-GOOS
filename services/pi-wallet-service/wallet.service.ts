e7650f8bbb3c13840016216a9873b5b098b53669
MOJAD - GOOS <eliekwizera030@gmail.com>
2026-06-26 12:28:52 +0200

    Create wallet.service.ts
    feat(wallet): production wallet foundation

---
+++ services/pi-wallet-service/wallet.service.ts
@@ -0,0 +1,19 @@
+wallet.service
+wallet info
+wallet sync
+balance.service
+balance
+pending
+available
+transaction.service
+deposits
+withdrawals
+transfers
+escrow.service
+escrow create
+release
+dispute
+rewards.service
+referral rewards
+creator rewards
+activity rewards
